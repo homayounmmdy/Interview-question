b03e95e817d3790cb7e59e30b3535a8755da5353
Homayoun <homayoun763@gmail.com>
2025-12-18 00:40:18 +0330

    update : github link

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,10 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span> Interview Questions</span>,
   project: {
-    link: 'https://github.com/homayunmmdy',
+    link: 'https://github.com/homayounmmdy',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Interview Questions',
   },
 }
 
